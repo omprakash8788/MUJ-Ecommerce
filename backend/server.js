671ed003de500303c107dd482f83eb0ce807b447
omprakash8788 <omprakashkumar552@gmail.com>
2025-04-13 13:00:20 +0530

    backend design

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Api working");
+});
+
+app.listen(port, () => console.log("Server stated on port : " + port));
