c07c8f710603154cb010565f9bb3c005615c0697
omprakash8788 <omprakashkumar552@gmail.com>
2025-04-21 19:40:51 +0530

    admin routing

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,32 @@
-import Navbar from "./components/Navbar"
-
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
 
 function App() {
-
   return (
+    <div className="bg-gray-50 min-h-screen">
+
     <>
-    <Navbar/>
+      <Navbar />
+      <hr />
+      <div className="flex w-full">
+        <Sidebar/>
+        <div className="w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base">
+          <Routes>
+            <Route path="/add" element={<Add/>}/>
+            <Route path="/list" element={<List/>}/>
+            <Route path="/orders" element={<Orders/>}/>
+
+          </Routes>
+        </div>
+      </div>
     </>
-  )
+
+    </div>
+  );
 }
 
-export default App
+export default App;
